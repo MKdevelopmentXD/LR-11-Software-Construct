8f4fde2d57630b4feeca3887b21caefe1be942ca
Clarence <clarencecallahan123456@gmail.com>
2023-03-28 19:58:53 +0300

    Output all runs

--- HelloWorldGit/Program.cs
+++ HelloWorldGit/Program.cs
@@ -1,14 +1,49 @@
 ﻿using System;
+using System.IO;
 
-namespace HelloWorldGit
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string fileName = "log.txt";
+        DateTime lastRun;
+
+        // Check if the user wants to print the log file
+        if (args.Length > 0 && args[0] == "all")
         {
-            Console.Write("Введiть ваше iм'я\n>>>");
-            String name = Console.ReadLine();
-            Console.WriteLine("Привіт " + name + ", поточна дата та час: " + DateTime.Now.ToString());
+            if (File.Exists(fileName))
+            {
+                Console.WriteLine("All runs:");
+                foreach (string line in File.ReadAllLines(fileName))
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No previous runs.");
+            }
+            return;
+        }
+
+        // Read the last run time from the file
+        if (File.Exists(fileName))
+        {
+            string lastRunStr = File.ReadAllText(fileName);
+            if (DateTime.TryParse(lastRunStr, out lastRun))
+            {
+                Console.WriteLine("Last run: {0}", lastRun);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No previous runs.");
+        }
+
+        // Write the current run time to the file
+        using (StreamWriter sw = File.AppendText(fileName))
+        {
+            sw.WriteLine(DateTime.Now.ToString());
         }
     }
 }
